4aed017d51ad39ff6cb515cd869f023fc76937f7
noahcote <noah.cote3@gmail.com>
2020-07-15 15:03:54 -0400

    updated to reflect heroku backend

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 
 export default class Navigation extends Component {
     render() {
